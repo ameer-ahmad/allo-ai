f0bf145f1458af0a225868c22ec31bd7c6db6e5a
ameer-ahmad <ameer5ahmad@gmail.com>
2022-05-18 18:04:58 -0400

    updated api key

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ function Footer() {
     }, [responses])
 
     const configuration = new Configuration({
-        apiKey: process.env.REACT_APP_API_KEY,
+        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
 
