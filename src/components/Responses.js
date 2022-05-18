8af312d855e6ce271884877850d190f50d13fcd4
ameer-ahmad <ameer5ahmad@gmail.com>
2022-05-18 17:13:55 -0400

    enabled auto scroll to bottom when new prompt is made

--- src/components/Responses.js
+++ src/components/Responses.js
@@ -1,15 +1,18 @@
 import React, {useContext} from 'react'
 import Response from './Response'
 import ResponseContext from '../ResponseContext';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 function Responses() {
     const {responses} = useContext(ResponseContext);
 
   return (
     <div className="responses">
+        <ScrollToBottom className="scrollBehavior">
         {responses.map((response, pos) => (
             <Response response={response} key={pos} />
         ))}
+        </ScrollToBottom>
     </div>
   )
 }
