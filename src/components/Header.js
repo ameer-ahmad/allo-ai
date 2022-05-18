5484c56463cc0baaf014358074dd5b6deacb9368
ameer-ahmad <ameer5ahmad@gmail.com>
2022-05-18 15:24:53 -0400

    moved clear button to header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import logo from '../images/logo.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faXmark} from '@fortawesome/free-solid-svg-icons';
+import ResponseContext from '../ResponseContext';
 
 function Header() {
+    const { setResponses } = useContext(ResponseContext);
+
   return (
     <div className="header">
         <div className="headerLogo">
             <img src={logo} alt="Allo logo" />
             <h1>Allo - Fun with AI</h1>
         </div>
-        <button>Clear Chat &nbsp; <FontAwesomeIcon className="headerIcon" icon={faXmark} /></button>
+        <button onClick={() => {
+            setResponses([])
+            window.localStorage.removeItem('RESPONSES');
+            }}>Clear &nbsp; <FontAwesomeIcon className="headerIcon" icon={faXmark} /></button>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,19 +4,6 @@ import Footer from './Footer';
 import Header from './Header';
 
 function Home() {
-    /*
-    useEffect(() => {
-        const data = window.localStorage.getItem('RESPONSES');
-        if (data !== null) setResponses(JSON.parse(data));
-    }, [])
-
-    useEffect(() => {
-        if (responses?.length) {
-            window.localStorage.setItem('RESPONSES', JSON.stringify(responses));
-        }
-    }, [responses])
-    */
-
   return (
         <div className="app">
             <Header/>
