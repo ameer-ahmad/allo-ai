4bdab082eb3656eef4d090f6f549913071d1cb5e
ameer-ahmad <ahmaamee@sheridancollege.ca>
2023-02-11 14:47:21 -0500

    Fixed issues with updated api

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React, { useState, useContext, useEffect } from 'react'
 import ResponseContext from '../ResponseContext';
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 
 function Footer() {
     const [prompt, setPrompt] = useState("");
     const {responses, setResponses} = useContext(ResponseContext);
-    const [engine, setEngine] = useState("");
+    const [model, setModel] = useState("");
 
     useEffect(() => {
         const data = window.localStorage.getItem('RESPONSES');
@@ -21,21 +21,22 @@ function Footer() {
     const configuration = new Configuration({
         apiKey: process.env.REACT_APP_OPENAI_API_KEY,
     });
+
     const openai = new OpenAIApi(configuration);
+    
 
     const submit = async () => {
         if (prompt !== "") {
-            if (engine !== "") {
+            if (model !== "") {
                 setPrompt("");
-                const response = await openai.createCompletion(engine, {
-                    prompt: prompt, 
-                    temperature: 0.8,
+                const response = await openai.createCompletion({
+                    model,
+                    prompt,
+                    temperature: 0,
                     max_tokens: 100,
-                    top_p: 1,
-                    frequency_penalty: 0,
-                    presence_penalty: 0,
-                })
-                setResponses([...responses, {prompt: prompt, response: response.data.choices[0].text, engine: engine}])
+                  });
+                console.log(response)
+                setResponses([...responses, {prompt: prompt, response: response.data.choices[0].text, engine: model}])
             } else {
                 alert("Please select an AI Engine")
             }
@@ -50,9 +51,9 @@ function Footer() {
     <div className="footer">
         <div className="footerSelect">
             <label>
-                <select name="engine" id="engine" onChange={(e) => setEngine(e.target.value)}>
+                <select name="engine" id="engine" onChange={(e) => setModel(e.target.value)}>
                 <option value="">Select AI Engine</option>
-                <option value="text-davinci-002">text-davinci-002</option>
+                <option value="text-davinci-003">text-davinci-003</option>
                 <option value="text-curie-001">text-curie-001</option>
                 <option value="text-babbage-001">text-babbage-001</option>
                 <option value="text-ada-001">text-ada-001</option>
